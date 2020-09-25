ef0807a33db9935ddee27a176794f4e7f2537e3c
hsmith <hal.smith@altran.com>
2020-09-25 10:03:59 +0100

    Fixed problem with draw multiplier being zero. Added disqualification if bot uses more than 100 dynamites.

--- dynamite/dynamite_runner.py
+++ dynamite/dynamite_runner.py
@@ -16,6 +16,9 @@ class DynamiteRunner:
         self.draw_multiplier = 1
         self.bot_1_wins = 0
         self.bot_2_wins = 0
+        self.draw_count = 0
+        self.bot_1_dynamite_count = 0
+        self.bot_2_dynamite_count = 0
         self.turn_count = 0
         self.win_map = self.load_win_map()
 
@@ -29,7 +32,14 @@ class DynamiteRunner:
             self.do_turn()
             if self.turn_count == MAX_COUNT:
                 break
-        print("bot_1: %i, bot_2: %i, turns: %i" % (self.bot_1_wins, self.bot_2_wins, self.turn_count))
+            if self.bot_1_dynamite_count > 100:
+                print("bot_1 disqualified for using too many dynamites")
+                break
+            if self.bot_2_dynamite_count > 100:
+                print("bot_2 disqualified for using too many dynamites")
+                break
+        print("bot_1: %i, bot_2: %i, draws: %i, turns: %i" % (self.bot_1_wins, self.bot_2_wins, self.draw_count,
+                                                              self.turn_count))
 
     def bot_reached_1000_wins(self):
         return self.bot_1_wins < WIN_COUNT and self.bot_2_wins < WIN_COUNT
@@ -37,17 +47,27 @@ class DynamiteRunner:
     def do_turn(self):
         bot_1_move = self.bot_1.make_move(self.move_dict)
         bot_2_move = self.bot_2.make_move(self.move_dict)
+
+        self.count_dynamite(bot_1_move, bot_2_move)
+
         self.move_dict['rounds'].append({'p1': bot_1_move, 'p2': bot_2_move})
         self.update_win_stats(bot_1_move, bot_2_move)
         self.turn_count += 1
+
+    def count_dynamite(self, bot_1_move, bot_2_move):
+        if bot_1_move == 'D':
+            self.bot_1_dynamite_count += 1
+        if bot_2_move == 'D':
+            self.bot_2_dynamite_count += 1
 
     def update_win_stats(self, bot_1_move, bot_2_move):
         win_id = self.win_map[bot_1_move][bot_2_move]
         if win_id == 'W':
             self.bot_1_wins += self.draw_multiplier
-            self.draw_multiplier = 0
+            self.draw_multiplier = 1
         elif win_id == 'L':
             self.bot_2_wins += self.draw_multiplier
-            self.draw_multiplier = 0
+            self.draw_multiplier = 1
         else:
+            self.draw_count += 1
             self.draw_multiplier += 1
